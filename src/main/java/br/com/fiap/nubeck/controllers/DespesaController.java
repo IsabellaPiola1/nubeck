91dc52b84fb7368f89d9deb11102a88514827b4e
isabellapiola1 <rm93960@fiap.com.br>
2023-03-14 17:10:23 -0300

    atualizacoes

--- src/main/java/br/com/fiap/nubeck/controllers/DespesaController.java
+++ src/main/java/br/com/fiap/nubeck/controllers/DespesaController.java
@@ -1,11 +1,23 @@
+package br.com.fiap.nubeck.controllers;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+//import java.math.BigDecimal;
+//import java.time.LocalDate;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.fiap.nubeck.models.Despesa;
+
 
 @RestController
 public class DespesaController {
 
     @GetMapping("/api/v1/despesas")
-    public String index(){
-        return new Despesa(1, new BigDecimal(100), localDate.now(), "cinema");
+    public Despesa index(){
+        return new Despesa(1l, new BigDecimal(100), LocalDate.now(), "cinema");
     }
     
 }
