020f44aae2e7e1456527112eaf58080b86ed461d
isabellapiola1 <rm93960@fiap.com.br>
2023-03-21 10:44:26 -0300

    insomnia

--- src/main/java/br/com/fiap/nubeck/controllers/DespesaController.java
+++ src/main/java/br/com/fiap/nubeck/controllers/DespesaController.java
@@ -1,12 +1,15 @@
 package br.com.fiap.nubeck.controllers;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
-//import java.math.BigDecimal;
-//import java.time.LocalDate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.nubeck.models.Despesa;
@@ -15,9 +18,19 @@ import br.com.fiap.nubeck.models.Despesa;
 @RestController
 public class DespesaController {
 
+    Logger log = LoggerFactory.getLogger(DespesaController.class);
+
+    List<Despesa> despesas = new ArrayList<>();
+
     @GetMapping("/api/v1/despesas")
-    public Despesa index(){
-        return new Despesa(1l, new BigDecimal(100), LocalDate.now(), "cinema");
+    public List<Despesa> index(){
+        return despesas;
+    }
+
+    @PostMapping("/api/v1/despesas")
+    public void create(@RequestBody Despesa despesa){
+        log.info("cadstrar despesa: " + despesa);
+        despesas.add(despesa);
     }
     
 }
